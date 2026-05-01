bd63cef720162f2b25d38d51495b1c485fcd0a8a
temirlan-fr <bolattimok197227@gmail.com>
2026-05-01 20:58:36 +0500

    Task 2 optimal shipping capacity

--- src/Main.java
+++ src/Main.java
@@ -53,35 +53,56 @@ public class Main {
 
 
 //        TASK-3
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Array size: ");
+//        int n = scanner.nextInt();
+//
+//        int[] arr = new int[n];
+//        System.out.print("Elements: ");
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length - 1 - i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//        }
+//
+//
+//        if (n % 2 == 1) {
+//            System.out.println("Median: " + arr[n / 2]);
+//        } else {
+//            double median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+//            System.out.println("Median: " + median);
+//        }
 
+
+
+
+
+//        TASK-4:
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Array size: ");
         int n = scanner.nextInt();
 
-        int[] arr = new int[n];
-        System.out.print("Elements: ");
+        int[] weights = new int[n];
+        System.out.print("Weights: ");
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            weights[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+        System.out.print("Days: ");
+        int days = scanner.nextInt();
 
-
-        if (n % 2 == 1) {
-            System.out.println("Median: " + arr[n / 2]);
-        } else {
-            double median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
-            System.out.println("Median: " + median);
-        }
+        int result = binarySearch(weights, days);
+        System.out.println("Minimum capacity: " + result);
     }
 
 //    public static boolean AnagramCheck(String word1, String word2){
@@ -114,4 +135,41 @@ public class Main {
 //        return Arrays.equals(arr1, arr2);
 //    }
 
+
+    public static int binarySearch(int[] weights, int days) {
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] > left) left = weights[i];
+            right += weights[i];
+        }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (canShip(weights, days, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static boolean canShip(int[] weights, int days, int capacity) {
+        int daysNeeded = 1;
+        int currentLoad = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            if (currentLoad + weights[i] > capacity) {
+                daysNeeded++;
+                currentLoad = 0;
+            }
+            currentLoad += weights[i];
+        }
+
+        return daysNeeded <= days;
+    }
 }
